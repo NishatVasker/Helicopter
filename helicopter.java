580d5ccf343d903195b0e39a598269477868c1dc
Nishat Vasker <83021162+NishatVasker@users.noreply.github.com>
2021-11-29 00:24:34 +0600

    Add files via upload

---
+++ helicopter.java
@@ -0,0 +1,167 @@
+
+    public class helicopter {
+        public static void main(String[] args) throws InterruptedException {
+
+            while (true) {
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("           :0000000000000000");
+                System.out.println("           A           ");
+                System.out.println("L      /--------       ");
+                System.out.println("LOL======      ()\\     ");
+                System.out.println("  L    \\         \\        ");
+                System.out.println("        \\_________]                ");
+                System.out.println("           I     I      ");
+                System.out.println("        -----------/              ");
+                System.out.println("                       ");
+
+
+                Thread.sleep(1000);
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("0000000000000000:         ");
+                System.out.println("                A           ");
+                System.out.println("     L      /--------       ");
+                System.out.println("     LOL======      ()\\     ");
+                System.out.println("            \\         \\        ");
+                System.out.println("             \\_________]                ");
+                System.out.println("                I     I      ");
+                System.out.println("             -----------/              ");
+                System.out.println("                            ");
+
+
+                Thread.sleep(1000);
+
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                       :00000000000000000         ");
+                System.out.println("                       A           ");
+                System.out.println("           L       /--------       ");
+                System.out.println("           LOL======      ()\\     ");
+                System.out.println("                   \\         \\        ");
+                System.out.println("                    \\_________]                ");
+                System.out.println("                       I     I      ");
+                System.out.println("                    -----------/              ");
+                System.out.println("                                   ");
+
+
+                Thread.sleep(1000);
+
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("              00000000000000000:         ");
+                System.out.println("                               A           ");
+                System.out.println("                   L        /--------       ");
+                System.out.println("                   LOL======      ()\\     ");
+                System.out.println("                           \\         \\        ");
+                System.out.println("                            \\_________]                ");
+                System.out.println("                               I     I      ");
+                System.out.println("                            -----------/              ");
+                System.out.println("                                           ");
+
+
+                Thread.sleep(1000);
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                                                :00000000000000000         ");
+                System.out.println("                                                A           ");
+                System.out.println("                                   L        /--------       ");
+                System.out.println("                                   LOL======      ()\\     ");
+                System.out.println("                                           \\         \\        ");
+                System.out.println("                                            \\_________]                ");
+                System.out.println("                                               I     I      ");
+                System.out.println("                                            -----------/              ");
+                System.out.println("                                                           ");
+
+
+                Thread.sleep(1000);
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                                       00000000000000000:         ");
+                System.out.println("                                                        A           ");
+                System.out.println("                                           L        /--------       ");
+                System.out.println("                                           LOL======      ()\\     ");
+                System.out.println("                                                   \\         \\        ");
+                System.out.println("                                                    \\_________]                ");
+                System.out.println("                                                       I     I      ");
+                System.out.println("                                                    -----------/              ");
+                System.out.println("                                                                   ");
+
+
+                Thread.sleep(1000);
+
+
+
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                                                                :00000000000000000         ");
+                System.out.println("                                                                A           ");
+                System.out.println("                                                   L        /--------       ");
+                System.out.println("                                                   LOL======      ()\\     ");
+                System.out.println("                                                           \\         \\        ");
+                System.out.println("                                                            \\_________]                ");
+                System.out.println("                                                               I     I      ");
+                System.out.println("                                                            -----------/              ");
+                System.out.println("                                                                           ");
+
+
+                Thread.sleep(1000);
+            }
+
+        }
+    }
